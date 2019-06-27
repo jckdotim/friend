abb0ddffb44973342f3fa3b2dad69c17da54644f
JC Kim <jck@linecorp.com>
2019-06-27 23:42:19 +0900

    feat: 🎸 add bmi function

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def factorial(n: int) -> int:
     return math.factorial(n)
 
 
+@f.skill
+def bmi(weight: int, height: int) -> float:
+    return weight / (height * 0.01) ** 2
+
+
 if __name__ == '__main__':
     while True:
         f.get_answer(input(f'{f.say()} >> '))
